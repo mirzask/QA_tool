8b0081dffe708580adcb2a28be99842d57bd39ec
Mirza Khan <mirzask@gmail.com>
2020-06-02 16:50:26 -0500

    reworked instanceN_fold; should work well now

--- QA_tool2.py
+++ QA_tool2.py
@@ -1,11 +1,16 @@
 import os
 import pandas as pd
+import pydicom
+from pathlib import Path
+
+
+"""
+Unused libraries:
 import nibabel as nib
 import re
 from glob import glob
-import pydicom
-from pathlib import Path
 from collections import Counter
+"""
 
 
 def mkdir(path):
@@ -32,7 +37,7 @@ def dcm_instance(dcm_root):
         print(
             "We were unable to find DICOM files in this root directory. Please review path and try again."
         )
-    slicePos = []
+    #slicePos = []
     instanceN = []
     for i in range(len(dcm_list)):
         ds = pydicom.dcmread(os.fspath(dcm_list[i]))
@@ -66,8 +71,7 @@ def dcm_slicedistance(dcm_root):
     #           skipped_accession.append(ds.AccessionNumber)
 
     #   print(f'The following accession numbers have missing "SliceLocation": {Counter(skipped_accession)}.')
-    ds_sorted = sorted(ds_list, reverse=True)
-    print(ds_sort == ds_sorted)
+    ds_sort = sorted(ds_list, reverse=True)
     res = 1
     for i in range(0, len(ds_sort) - 2):
         print((ds_sort[i] - ds_sort[i + 1]), (ds_sort[i + 1] - ds_sort[i + 2]))
@@ -78,9 +82,10 @@ def dcm_slicedistance(dcm_root):
     return res
 
 
-#dcm_slicedistance("/Users/sami/Desktop/test/")
+# dcm_slicedistance("/Users/sami/Desktop/test/")
 
-def instanceN_fold(fold_root, save_csv_path='instance_num_check.csv'):  # instanceN_fold
+
+def instanceN_fold(fold_root, save_csv_path="instance_num_check.csv"):  # instanceN_fold
     """
     Arguments:
         - Root folder
@@ -99,29 +104,31 @@ def instanceN_fold(fold_root, save_csv_path='instance_num_check.csv'):  # instan
         sess_list = [x.stem for x in Path(subj_path).iterdir() if x.is_dir()]
         for j in range(len(sess_list)):
             sess.append(sess_list[j])
-            #print("(i, j): ", i, j, sess_list[j])
+            # print("(i, j): ", i, j, sess_list[j])
             sess_path = subj_path / sess_list[j]
-            instance_list =  [x.stem for x in Path(sess_path).iterdir() if x.is_dir()]
+            instance_list = [x.stem for x in Path(sess_path).iterdir() if x.is_dir()]
             if len(instance_list) == 1:
                 single_folder.append(1)
             else:
                 single_folder.append(0)
             size_list = []
             for k in range(len(instance_list)):
-                #print(sess_path / instance_list[k])
-                #if (sess_path / instance_list[k] / "secondary").exists() and not (sess_path / instance_list[k] / "DICOM").exists():    # Unnecessary if not dealing with DICOM subdir
+                p = sess_path / instance_list[k]
+                size_list.append(len(list(p.rglob("*.dcm"))))
+                # print(sess_path / instance_list[k])
+                # if (sess_path / instance_list[k] / "secondary").exists() and not (sess_path / instance_list[k] / "DICOM").exists():    # Unnecessary if not dealing with DICOM subdir
                 #    (sess_path / instance_list[k] / "secondary").rename(sess_path / instance_list[k] / "DICOM")
                 # size = len(os.listdir(sess_path + "/" + instance_list[k] + "/DICOM")) # There is no DICOM subdirectory, so this throws an error
-                size = len([x for x in (sess_path / instance_list[k]).iterdir() if x.is_dir()])
-                size_list.append(size)
+                # size = len([x for x in  if x.is_dir()])
+                # size_list.append(size)
             max_index = size_list.index(max(size_list))
             # break
-            os.rename(
-                sess_path + "/" + instance_list[max_index], sess_path + "/new_max"
-            )
+
+            # Renames the dir with the greatest # of dcm image files
+            (sess_path / instance_list[max_index]).rename(sess_path / "new_max")
             try:
                 # inst_n, dicom_n, same = dcm_instance(sess_path + "/new_max/DICOM") # Again, there is no DICOM subdirectory, so this throws an error
-                inst_n, dicom_n, same = dcm_instance(sess_path + "/new_max")
+                inst_n, dicom_n, same = dcm_instance(sess_path / "new_max")
                 instanceN.append(inst_n)
                 dicomN.append(dicom_n)
                 diff.append(same)
@@ -139,4 +146,4 @@ def instanceN_fold(fold_root, save_csv_path='instance_num_check.csv'):  # instan
     data.to_csv(save_csv_path, index=False)
 
 
-instanceN_fold("/Users/sami/Desktop/test/", "/Users/sami/Desktop/test/instance_num_check.csv")
+# instanceN_fold("/Users/sami/Desktop/test/", "/Users/sami/Desktop/test/instance_num_check.csv")
